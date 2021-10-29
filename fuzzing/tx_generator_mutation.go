115ce5b2b7d06cefa0fd59afe630550f9016f7c4
Spencer Michaels <spencer.michaels@trailofbits.com>
2021-10-29 15:29:07 -0500

    add string and byte mutations

--- fuzzing/tx_generator_mutation.go
+++ fuzzing/tx_generator_mutation.go
@@ -41,24 +41,6 @@ func newTxGeneratorMutation(corpus *Corpus) *txGeneratorMutation {
 	return generator
 }
 
-// generateInteger generates/selects an integer to use when populating transaction fields.
-func (g *txGeneratorMutation) generateInteger(worker *fuzzerWorker, signed bool, bitLength int) *big.Int {
-	// Calculate our integer bounds
-	min, max := utils.GetIntegerConstraints(signed, bitLength)
-
-	// Determine additional inputs based off the value type
-	var inputs []*big.Int
-	inputs = append(inputs,  g.baseIntegers...)
-	if signed {
-		inputs = append(inputs, big.NewInt(0), big.NewInt(1), big.NewInt(-1), big.NewInt(2), min, max)
-	} else {
-		inputs = append(inputs, big.NewInt(1), big.NewInt(2), min, max) // zero is included in minimum
-	}
-
-	// Generate a mutated integer.
-	return g.generateMutatedInteger(worker, min, max, inputs)
-}
-
 // integerMutationMethods define methods which take a big integer and a set of inputs and
 // transform the integer with a random input and operation. This is used in a loop to create
 // mutated integer values.
@@ -93,20 +75,76 @@ var integerMutationMethods = []func(*txGeneratorMutation, *big.Int, ...*big.Int)
 	},
 }
 
-// generateMutatedInteger generates an integer of the specified type by taking a starting value and mutating it
-// such that it is derived from a sensible value and may reach some code paths more effectively, but it is still
-// sufficiently randomized.
-// Returns the generated mutated integer.
-func (g *txGeneratorMutation) generateMutatedInteger(worker *fuzzerWorker, min *big.Int, max *big.Int, inputs []*big.Int) *big.Int {
-	// Declare additional inputs for our integer to mutate and select one.
-	// Note: We add some basic cases as well.
+var bytesMutationMethods = []func(*txGeneratorMutation, []byte, ...[]byte) []byte {
+	// Replace a random index with a random byte
+	func(g *txGeneratorMutation, b []byte, inputs ...[]byte ) []byte {
+		b[g.randomProvider.Int() % len(b)] = byte(g.randomProvider.Int() % 256)
+		return b
+	},
+	// Append a random byte to the front
+	func(g *txGeneratorMutation, b []byte, inputs ...[]byte ) []byte {
+		return append([]byte{byte(g.randomProvider.Int() % 256)}, b[:]...)
+	},
+	// Append a random byte to the end
+	func(g *txGeneratorMutation, b []byte, inputs ...[]byte ) []byte {
+		return append(b, byte(g.randomProvider.Int() % 256))
+	},
+	// Remove a random byte
+	func(g *txGeneratorMutation, b []byte, inputs ...[]byte ) []byte {
+		i := g.randomProvider.Int() % len(b)
+		return append(b[:i], b[i+1:]...)
+	},
+}
 
+var stringMutationMethods = []func(*txGeneratorMutation, string, ...string) string {
+	// Replace a random index with a random character
+	func(g *txGeneratorMutation, s string, inputs ...string) string {
+		r := []rune(s)
+		r[g.randomProvider.Int() % len(s)] = rune(32 + g.randomProvider.Int() % 95)
+		return string(r)
+	},
+	// Append a random character to the front
+	func(g *txGeneratorMutation, s string, inputs ...string) string {
+		return s + string(32 + g.randomProvider.Int() % 95)
+	},
+	// Append a random character to the end
+	func(g *txGeneratorMutation, s string, inputs ...string) string {
+		return string(32 + g.randomProvider.Int() % 95) + s
+	},
+	// Remove a random character
+	func(g *txGeneratorMutation, s string, inputs ...string) string {
+		i := g.randomProvider.Int() % len(s)
+		return s[:i] + s[i+1:]
+	},
+}
+
+func (g *txGeneratorMutation) getMutationParams(inputsLen int) (int, int) {
 	g.randomProviderLock.Lock()
-	input := inputs[g.randomProvider.Int() % len(inputs)]
-
-	// Determine how many times to mutate our integer and begin each pass
+	inputIdx := g.randomProvider.Int() % inputsLen
 	mutationCount := g.randomProvider.Int() % (g.maxMutationRounds + 1)
 	g.randomProviderLock.Unlock()
+
+	return inputIdx, mutationCount
+}
+
+
+// generateInteger generates/selects an integer to use when populating transaction fields.
+func (g *txGeneratorMutation) generateInteger(worker *fuzzerWorker, signed bool, bitLength int) *big.Int {
+	// Calculate our integer bounds
+	min, max := utils.GetIntegerConstraints(signed, bitLength)
+
+	// Determine additional inputs based off the value type
+	var inputs []*big.Int
+	inputs = append(inputs,  g.baseIntegers...)
+	if signed {
+		inputs = append(inputs, big.NewInt(0), big.NewInt(1), big.NewInt(-1), big.NewInt(2), min, max)
+	} else {
+		inputs = append(inputs, big.NewInt(1), big.NewInt(2), min, max) // zero is included in minimum
+	}
+
+	inputIdx, mutationCount := g.getMutationParams(len(inputs))
+	input := inputs[inputIdx]
+
 	for i := 0; i < mutationCount; i++ {
 		// Mutate input
 		g.randomProviderLock.Lock()
@@ -116,5 +154,33 @@ func (g *txGeneratorMutation) generateMutatedInteger(worker *fuzzerWorker, min *
 		// Correct value boundaries (underflow/overflow)
 		input = utils.ConstrainIntegerToBounds(input, min, max)
 	}
+	return input
+}
+
+func (g *txGeneratorMutation) generateBytes(worker *fuzzerWorker) []byte {
+	inputs := g.baseBytes
+	inputIdx, mutationCount := g.getMutationParams(len(inputs))
+	input := inputs[inputIdx]
+
+	for i := 0; i < mutationCount; i++ {
+		g.randomProviderLock.Lock()
+		input = bytesMutationMethods[g.randomProvider.Int() % len(bytesMutationMethods)](g, input, inputs...)
+		g.randomProviderLock.Unlock()
+	}
+
+	return input
+}
+
+func (g *txGeneratorMutation) generateString(worker *fuzzerWorker) string {
+	inputs := g.baseStrings
+	inputIdx, mutationCount := g.getMutationParams(len(inputs))
+	input := inputs[inputIdx]
+
+	for i := 0; i < mutationCount; i++ {
+		g.randomProviderLock.Lock()
+		input = stringMutationMethods[g.randomProvider.Int() % len(stringMutationMethods)](g, input, inputs...)
+		g.randomProviderLock.Unlock()
+	}
+
 	return input
 }
