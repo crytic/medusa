afce75c0e00e0311048fd0d4637a9082e354b9ba
Anish Naik <anish.naik@trailofbits.com>
2022-08-16 16:13:07 -0400

    Initial commit for crytic-compile support
    - This is a baseline that requires a lot of re-work
    - The difficult aspect is figuring out how to properly parse `standard` crytic-compile output and store it properly
    
    TODOS
    - Still need to figure out `sourcePath` and `source` is (see truffle.go)
    - Why is `asts` plural in `standard` formatting. Do we have to set the same AST for each contract
    - Make sure configuration units are parsed properly

---
+++ compilation/platforms/crytic_compile_test.go
@@ -0,0 +1,45 @@
+package platforms
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"path"
+	"testing"
+)
+
+func TestSimpleCryticCompileCompilation(t *testing.T) {
+	// Define our contract source code
+	contractSource := `
+contract SimpleCryticCompilation {
+    uint x1;
+    uint x2;
+
+    function setx1(uint val) public {
+        x1 = val;
+    }
+
+    function setx2(uint val) public {
+        x2 = val;
+    }
+
+    function medusa_set_x1_x2_sequence() public view returns (bool) {
+        return x1 != x2 * 3 || x1 == 0;
+    }
+}`
+
+	// Write the contract out to our temporary test directory
+	tempDir := t.TempDir()
+	contractPath := path.Join(tempDir, "simple_crytic_compilation.sol")
+	err := ioutil.WriteFile(contractPath, []byte(contractSource), 0777)
+	assert.Nil(t, err)
+
+	// Create a solc provider
+	fmt.Printf("directory is %s\n", tempDir)
+	cryticConfig := NewCryticCompileCompilationConfig(contractPath)
+
+	// Obtain our solc version and ensure we didn't encounter an error
+	compilations, _, err := cryticConfig.Compile()
+	assert.Nil(t, err)
+	assert.True(t, len(compilations) > 0)
+}
