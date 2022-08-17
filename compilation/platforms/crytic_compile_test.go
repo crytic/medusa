446cae906f1cb88faf06aff3a910e5b7ebc94db3
Anish Naik <anish.naik@trailofbits.com>
2022-08-17 12:22:27 -0400

    More unit tests for crytic-compile
    Total of 3 unit tests
    - Single file with no additional args
    - Single file with export-format, export-dir, and bad-arg...all should fail
    - Whole directory with no additional args
    
    A few more permutations left but this tests the base concerns

--- compilation/platforms/crytic_compile_test.go
+++ compilation/platforms/crytic_compile_test.go
@@ -1,45 +1,123 @@
 package platforms
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"path"
 	"testing"
 )
 
-func TestSimpleCryticCompilation(t *testing.T) {
-	// Define our contract source code
-	contractSource := `
-contract SimpleCryticCompilation {
-    uint x1;
-    uint x2;
-
-    function setx1(uint val) public {
-        x1 = val;
-    }
-
-    function setx2(uint val) public {
-        x2 = val;
-    }
-
-    function medusa_set_x1_x2_sequence() public view returns (bool) {
-        return x1 != x2 * 3 || x1 == 0;
-    }
-}`
-
+func SetupContracts(t *testing.T, isFile bool, twoContracts bool) string {
+	contractOne := `
+	contract ContractOne {
+		uint x1;
+		uint x2;
+	
+		function setx1(uint val) public {
+			x1 = val;
+		}
+	
+		function setx2(uint val) public {
+			x2 = val;
+		}
+	
+		function medusa_set_x1_x2_sequence() public view returns (bool) {
+			return x1 != x2 * 3 || x1 == 0;
+		}
+	}`
+	contractTwo := `
+	abstract contract AbstractContractTwo {
+		uint x;		
+		function setx(uint val) public {
+			x = val;
+		}
+	}
+	contract ContractTwo {
+		uint x1;
+		uint x2;
+	
+		function setx1(uint val) public {
+			x1 = val;
+		}
+	
+		function setx2(uint val) public {
+			x2 = val;
+		}
+	
+		function medusa_set_x1_x2_sequence() public view returns (bool) {
+			return x1 != x2 * 3 || x1 == 0;
+		}
+	}`
 	// Write the contract out to our temporary test directory
 	tempDir := t.TempDir()
-	contractPath := path.Join(tempDir, "simple_crytic_compilation.sol")
-	err := ioutil.WriteFile(contractPath, []byte(contractSource), 0777)
+	contractPathOne := path.Join(tempDir, "crytic_one.sol")
+	err := ioutil.WriteFile(contractPathOne, []byte(contractOne), 0644)
 	assert.Nil(t, err)
+	if twoContracts {
+		contractPathTwo := path.Join(tempDir, "crytic_two.sol")
+		err := ioutil.WriteFile(contractPathTwo, []byte(contractTwo), 0644)
+		assert.Nil(t, err)
+	}
+	if isFile {
+		return contractPathOne
+	}
+	return tempDir
+}
 
-	// Create a solc provider
-	fmt.Printf("directory is %s\n", tempDir)
-	cryticConfig := NewCryticCompilationConfig(contractPath)
+// Single file test with no additional args
+func TestCryticSingleFileNoArgs(t *testing.T) {
+	// Setup contract
+	isFile := true
+	twoContracts := false
+	filePath := SetupContracts(t, isFile, twoContracts)
 
-	// Obtain our solc version and ensure we didn't encounter an error
+	// Create a crytic-compile provider
+	cryticConfig := NewCryticCompilationConfig(filePath)
+
 	compilations, _, err := cryticConfig.Compile()
+	// No failures
 	assert.Nil(t, err)
-	assert.True(t, len(compilations) > 0)
+	assert.True(t, len(compilations) == 1)                                        // One compilation object
+	assert.True(t, len(compilations[0].Sources) == 1)                             // One source because we specified one file
+	assert.True(t, len(compilations[0].Sources["crytic_one.sol"].Contracts) == 1) // One contract in crytic.sol
+}
+
+// Single file test with bad arguments
+func TestCryticSingleFileBadArgs(t *testing.T) {
+	// Setup contract
+	isFile := true
+	twoContracts := false
+	filePath := SetupContracts(t, isFile, twoContracts)
+
+	// Create a crytic-compile provider
+	cryticConfig := NewCryticCompilationConfig(filePath)
+	// Make sure --export-format and --export-dir are not allowed
+	cryticConfig.Args = append(cryticConfig.Args, "--export-format")
+	_, _, err := cryticConfig.Compile()
+	// Should fail
+	assert.Error(t, err)
+	cryticConfig.Args = append(cryticConfig.Args, "--export-dir")
+	_, _, err = cryticConfig.Compile()
+	assert.Error(t, err)
+	cryticConfig.Args = append(cryticConfig.Args, "--bad-arg")
+	_, _, err = cryticConfig.Compile()
+	assert.Error(t, err)
+}
+
+// Whole directory test with no args
+func TestCryticDirectoryNoArgs(t *testing.T) {
+	// Setup contracts
+	isFile := false
+	twoContracts := true
+	filePath := SetupContracts(t, isFile, twoContracts)
+
+	// Create a crytic-compile provider
+	cryticConfig := NewCryticCompilationConfig(filePath)
+	compilations, _, err := cryticConfig.Compile()
+	// No failures
+	assert.Nil(t, err)
+	assert.True(t, len(compilations) == 1)                                        // One compilation object
+	assert.True(t, len(compilations[0].Sources) == 2)                             // Two sources because we specified two files
+	assert.True(t, len(compilations[0].Sources["crytic_one.sol"].Contracts) == 1) // One contract in crytic_one.sol
+	assert.True(t, len(compilations[0].Sources["crytic_two.sol"].Contracts) == 2) // Two contracts in crytic_two.sol
 }
