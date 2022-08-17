630323cfc79a5e1ab2082f1379dd5c3250bd26df
Anish Naik <anish.naik@trailofbits.com>
2022-08-16 20:42:27 -0400

    Decreased nesting complexity
    1. Removed a lot of the nesting complexity while parsing standard crytic-compile output
    2. Removed old print statements
    3. Added better error handling for debugging

--- compilation/platforms/crytic_compile.go
+++ compilation/platforms/crytic_compile.go
@@ -71,7 +71,6 @@ func (s *CryticCompileCompilationConfig) Compile() ([]types.Compilation, string,
 	if buildDirectory == "" {
 		buildDirectory = path.Join(parentDirectory, "crytic-export")
 	}
-	fmt.Printf("Build directory is %s\n", buildDirectory)
 	matches, err := filepath.Glob(path.Join(buildDirectory, "*.json"))
 	if err != nil {
 		return nil, "", err
@@ -80,31 +79,10 @@ func (s *CryticCompileCompilationConfig) Compile() ([]types.Compilation, string,
 	// Create a compilation unit out of this.
 	compilation := types.NewCompilation()
 
-	// Define various structures to parse `standard` formatting
-	/*
-		type ContractData struct {
-			Abi               interface{} `json:"abi"`
-			Bytecode          string      `json:"bin"`
-			DeployedBytecode  string      `json:"bin-runtime"`
-			SourceMap         string      `json:"srcmap"`
-			DeployedSourceMap string      `json:"srcmap-runtime"`
-		}
-		type ContractMapMap map[string]ContractData
-		type ContractMap map[string]ContractMapMap
-		type CompilationUnit struct {
-			Asts      interface{} `json:"asts"`
-			Contracts ContractMap `json:"contracts"`
-		}
-		type CompilationUnitMap map[string]CompilationUnit
-		type CryticCompileCompiledJson struct {
-			CompilationUnits CompilationUnitMap `json:"compilation_units"`
-		}*/
-
-	// Loop for each truffle artifact to parse our compilations.
+	// Loop for each crytic artifact to parse our compilations.
 	for i := 0; i < len(matches); i++ {
 		// Read the compiled JSON file data
 		b, err := ioutil.ReadFile(matches[i])
-		//fmt.Printf("b is %s\n", b)
 		if err != nil {
 			return nil, "", err
 		}
@@ -115,108 +93,66 @@ func (s *CryticCompileCompilationConfig) Compile() ([]types.Compilation, string,
 			return nil, "", err
 		}
 		// Index into "compilation_units" key
-		if compilationUnits, ok := compiledJson["compilation_units"]; ok {
-			// fmt.Printf("comp units are %s\n", compilationUnits)
-			// Create a mapping between key (filename) and value (contract and ast information) each compilation unit
-			if compilationMap, ok := compilationUnits.(map[string]interface{}); ok {
-				for _, contractsAndAst := range compilationMap {
-					// fmt.Printf("%s\n", contractAndAst)
-					// Create mapping between key (compiler / asts / contracts) and associated values
-					if contractsAndAstMap, ok := contractsAndAst.(map[string]interface{}); ok {
-						Ast := contractsAndAstMap["asts"]
-						// Create mapping between key (file name) and value (associated contracts in that file)
-						if contractsMap, ok := contractsAndAstMap["contracts"].(map[string]interface{}); ok {
-							// Iterate through each contract FILE
-							for fileName, contractsData := range contractsMap {
-								//fmt.Printf("%s\n", contractName)
-								// fmt.Printf("%s\n", contractData)
-								// Create mapping between all contracts in a file (key) to it's data (abi, etc.)
-								if contractMap, ok := contractsData.(map[string]interface{}); ok {
-									// Iterate through each contract
-									for contractName, contractData := range contractMap {
-										// Create unique source path
-										sourcePath := fileName + ":" + contractName
-										// Create mapping between contract details (abi, bytecode) to actual values
-										if contractDataMap, ok := contractData.(map[string]interface{}); ok {
-											contractAbi, err := types.InterfaceToABI(contractDataMap["abi"])
-											if err != nil {
-												continue
-											}
-											// Check if source is already in compilation object
-											if _, ok := compilation.Sources[sourcePath]; !ok {
-												compilation.Sources[sourcePath] = types.CompiledSource{
-													Ast:       Ast,
-													Contracts: make(map[string]types.CompiledContract),
-												}
-											}
-											compilation.Sources[sourcePath].Contracts[contractName] = types.CompiledContract{
-												Abi:             *contractAbi,
-												RuntimeBytecode: fmt.Sprintf("%v", contractDataMap["bin-runtime"]),
-												InitBytecode:    fmt.Sprintf("%v", contractDataMap["bin"]),
-												SrcMapsInit:     fmt.Sprintf("%v", contractDataMap["srcmap"]),
-												SrcMapsRuntime:  fmt.Sprintf("%v", contractDataMap["srcmap-runtime"]),
-											}
-										}
-									}
-								}
-							}
+		compilationUnits, ok := compiledJson["compilation_units"]
+		if !ok {
+			return nil, "", fmt.Errorf("error while parsing compiledJson: %s\n", compiledJson)
+		}
+		// Create a mapping between key (filename) and value (contract and ast information) each compilation unit
+		compilationMap, ok := compilationUnits.(map[string]interface{})
+		if !ok {
+			return nil, "", fmt.Errorf("error while parsing compilationUnits: %s\n", compilationUnits)
+		}
+		for _, contractsAndAst := range compilationMap {
+			// Create mapping between key (compiler / asts / contracts) and associated values
+			contractsAndAstMap, ok := contractsAndAst.(map[string]interface{})
+			if !ok {
+				return nil, "", fmt.Errorf("error while parsing contractsAndAst: %s\n", contractsAndAst)
+			}
+			Ast := contractsAndAstMap["asts"]
+			// Create mapping between key (file name) and value (associated contracts in that file)
+			contractsMap, ok := contractsAndAstMap["contracts"].(map[string]interface{})
+			if !ok {
+				return nil, "", fmt.Errorf("error while parsing contractsAndAstMap: %s\n", contractsAndAstMap)
+			}
+			// Iterate through each contract FILE
+			for fileName, contractsData := range contractsMap {
+				// Create mapping between all contracts in a file (key) to it's data (abi, etc.)
+				contractMap, ok := contractsData.(map[string]interface{})
+				if !ok {
+					return nil, "", fmt.Errorf("error while parsing contractsData: %s\n", contractsData)
+				}
+				// Iterate through each contract
+				for contractName, contractData := range contractMap {
+					// Create unique source path
+					sourcePath := fileName + ":" + contractName
+					// Create mapping between contract details (abi, bytecode) to actual values
+					contractDataMap, ok := contractData.(map[string]interface{})
+					if !ok {
+						return nil, "", fmt.Errorf("error while parsing contractData: %s\n", contractData)
+					}
+					contractAbi, err := types.InterfaceToABI(contractDataMap["abi"])
+					if err != nil {
+						// TODO: Throw error here?
+						continue
+					}
+					// Check if source is already in compilation object
+					if _, ok := compilation.Sources[sourcePath]; !ok {
+						compilation.Sources[sourcePath] = types.CompiledSource{
+							Ast:       Ast,
+							Contracts: make(map[string]types.CompiledContract),
 						}
 					}
+					compilation.Sources[sourcePath].Contracts[contractName] = types.CompiledContract{
+						Abi:             *contractAbi,
+						RuntimeBytecode: fmt.Sprintf("%v", contractDataMap["bin-runtime"]),
+						InitBytecode:    fmt.Sprintf("%v", contractDataMap["bin"]),
+						SrcMapsInit:     fmt.Sprintf("%v", contractDataMap["srcmap"]),
+						SrcMapsRuntime:  fmt.Sprintf("%v", contractDataMap["srcmap-runtime"]),
+					}
+
 				}
 			}
 		}
-
-		/*
-			for relativeFilePath, _ := range compiledJson.CompilationUnits {
-				ast := compiledJson.CompilationUnits[relativeFilePath].Asts
-				for fileName, _ := range compiledJson.CompilationUnits[relativeFilePath].Contracts {
-					for contractName, _ := range compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName] {
-						fmt.Printf("something is %s\n", compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].Abi)
-						contractAbi, err := types.InterfaceToABI(compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].Abi)
-						if err != nil {
-							continue
-						}
-						compilation.Sources[contractName] = types.CompiledSource{
-							Ast:       ast,
-							Contracts: make(map[string]types.CompiledContract),
-						}
-						compilation.Sources[contractName].Contracts[contractName] = types.CompiledContract{
-							Abi:             *contractAbi,
-							RuntimeBytecode: compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].DeployedBytecode,
-							InitBytecode:    compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].Bytecode,
-							SrcMapsInit:     compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].SourceMap,
-							SrcMapsRuntime:  compiledJson.CompilationUnits[relativeFilePath].Contracts[fileName][contractName].DeployedSourceMap,
-						}
-					}
-				}
-
-		*/
-		//}
-		/*
-			fmt.Printf("contract name is %s\n", compiledJson.ContractName)
-			// Convert the abi structure to our parsed abi type
-			contractAbi, err := types.InterfaceToABI(compiledJson.Abi)
-			if err != nil {
-				continue
-			}
-
-			// If we don't have a source for this file, create it.
-			if _, ok := compilation.Sources[compiledJson.SourcePath]; !ok {
-				compilation.Sources[compiledJson.SourcePath] = types.CompiledSource{
-					Ast:       compiledJson.Ast,
-					Contracts: make(map[string]types.CompiledContract),
-				}
-			}
-
-			// Add our contract to the source
-			compilation.Sources[compiledJson.SourcePath].Contracts[compiledJson.ContractName] = types.CompiledContract{
-				Abi:             *contractAbi,
-				RuntimeBytecode: compiledJson.DeployedBytecode,
-				InitBytecode:    compiledJson.Bytecode,
-				SrcMapsInit:     compiledJson.SourceMap,
-				SrcMapsRuntime:  compiledJson.DeployedSourceMap,
-			}
-		*/
 	}
 	return []types.Compilation{*compilation}, string(out), nil
 }
