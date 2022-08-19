e34fc827c4ed5c658fe1e33ec175f0f2dbf45cd9
Anish Naik <anish.naik@trailofbits.com>
2022-08-19 10:19:25 -0400

    Improved solc output options selection
    The output options allowed during solc compilation is a function of what solc version is available. This commit repurposes the code from crytic-compile here: https://github.com/crytic/crytic-compile/blob/master/crytic_compile/platform/solc.py#L402-430 to choose which outputOptions to provide.
    
    Note that the above snippet has a minor bug on line 416, it should be range(12, 27).

--- compilation/platforms/solc.go
+++ compilation/platforms/solc.go
@@ -44,6 +44,28 @@ func GetSystemSolcVersion() (*semver.Version, error) {
 	return semver.NewVersion(versionStr)
 }
 
+// SetSolcOutputOptions determines what outputOptions should be provided to solc given a semver.Version
+func (s *SolcCompilationConfig) SetSolcOutputOptions(v *semver.Version) string {
+	// TODO: should we remove patch conditionals if all patch versions are supported for a given minor version?
+	// useCompactFormat will add the compact-format output option
+	// if version is 0.4.12-0.4.26 or 0.5.0-0.5.17 or 0.6.0-0.6.12 or 0.7.0-0.7.6 or 0.8.0-0.8.9
+	useCompactFormat := (v.Major() == 0 && v.Minor() == 4 && v.Patch() >= 12 && v.Patch() <= 26) ||
+		(v.Major() == 0 && v.Minor() == 5 && v.Patch() <= 17) ||
+		(v.Major() == 0 && v.Minor() == 6 && v.Patch() <= 12) ||
+		(v.Major() == 0 && v.Minor() == 7 && v.Patch() <= 6) ||
+		(v.Major() == 0 && v.Minor() == 8 && v.Patch() <= 9)
+
+	// if version is 0.3.0-0.3.6 or 0.4.0-0.4.11 no 'hashes' outputOption
+	if (v.Major() == 0 && v.Minor() == 4 && v.Patch() <= 11) || (v.Major() == 0 && v.Minor() == 3 && v.Patch() <= 6) {
+		return "abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc"
+	} else if useCompactFormat {
+		// Both 'hashes' and 'compact-format' are allowed as outputOptions
+		return "abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc,hashes,compact-format"
+	} else {
+		// Can't use 'compact-format' but 'hashes' is allowed as outputOption
+		return "abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc,hashes"
+	}
+}
 func (s *SolcCompilationConfig) Compile() ([]types.Compilation, string, error) {
 	// Obtain our solc version string
 	v, err := GetSystemSolcVersion()
@@ -52,12 +74,7 @@ func (s *SolcCompilationConfig) Compile() ([]types.Compilation, string, error) {
 	}
 
 	// Determine which compiler options we need.
-	var outputOptions string
-	if (v.Major() == 0 && v.Minor() == 3) || (v.Major() == 0 && v.Minor() == 4 && v.Patch() <= 12) {
-		outputOptions = "abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc"
-	} else {
-		outputOptions = "abi,ast,bin,bin-runtime,srcmap,srcmap-runtime,userdoc,devdoc,hashes,compact-format"
-	}
+	outputOptions := s.SetSolcOutputOptions(v)
 
 	// Execute solc to compile our target.
 	out, err := exec.Command("solc", s.Target, "--combined-json", outputOptions).Output()
