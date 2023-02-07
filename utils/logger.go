a23c6a8e32555b715285cc939cd8575f3e2200aa
bohendo <bohendo@skiff.com>
2023-02-07 16:07:03 -0500

    use slog built-in types

--- utils/logger.go
+++ utils/logger.go
@@ -1,40 +1,81 @@
 package utils
+
 import (
-  "fmt"
-  "golang.org/x/exp/slog"
+  "os"
+	"golang.org/x/exp/slog"
 )
 
-// using any type for print args because that's what Printf uses, see:
-// https://cs.opensource.google/go/go/+/master:src/fmt/print.go;l=232
-
+/*
+type LogLevel int64
+const (
+	Unknown LogLevel = iota
+	Error
+	Warn
+	Info
+	Debug
+)
+type LogFormat string
+const (
+	Text LogFormat = "Text" // one line with key=value pairs
+	JSON LogFormat = "JSON" // one line in JSON format
+)
+type LogDestination string
+const (
+	Standard LogDestination = "Standard" // stdout
+	File LogDestination = "File" // write to file on disk
+	Return LogDestination = "Return" // return for further processing
+)
 type Logger struct {
-  Level string "info"
+	// indicates where in the system this log came from
+	Context string "NoContext"
+	// all logs with a severity less than MaxLevel will be silently ignored
+	MaxLevel LogLevel "Info"
+  Format LogFormat "Text"
+  Destination LogDestination "Standard"
 }
+*/
 
-func (l *Logger) GetLevel() string {
-  return l.Level
-}
-
-func NewLogger(level string) *Logger {
-  return &Logger{
-    Level: level,
+func NewLogger(
+  format string,
+  maxlevel int8,
+) *slog.Logger {
+  var handler slog.Handler
+  if format == "JSON" {
+    handler = slog.NewJSONHandler(os.Stdout)
+  } else {
+    handler = slog.NewTextHandler(os.Stdout)
   }
+  logger := slog.New(handler)
+  return logger
+  /*
+	return &Logger{
+		Context:  context,
+		MaxLevel: maxlevel,
+    Destination: destination,
+    Format: format,
+	}
+  */
 }
 
-func Debug(baseMsg string, args ...any) {
+// using any type for print args because that's what Printf uses, see: https://cs.opensource.google/go/go/+/master:src/fmt/print.go;l=232
+// args should be alternating key names (string) and values (any)
+//   an odd number of args should ignore the dangling key name (TBD?)
+/*
+func (l *slog.Logger) Debug(baseMsg string, args ...any) {
 	slog.Debug(fmt.Sprintf(baseMsg, args...))
 }
 
+func (l *slog.Logger) Info(baseMsg string, args ...any) {
+	slog.Info(fmt.Sprintf(baseMsg, args...))
+}
+
+func (l *slog.Logger) Warn(baseMsg string, args ...any) {
+	slog.Warn(fmt.Sprintf(baseMsg, args...))
+}
+*/
+
 /*
-func Info(baseMsg string, args ...any) {
-	slog.Info(fmt.Sprintf(baseMsg, args))
-}
-
-func Warn(baseMsg string, args ...any) {
-	slog.Warn(fmt.Sprintf(baseMsg, args))
-}
-
 func Error(baseMsg string, args ...any) {
-	slog.Error(fmt.Sprintf(baseMsg, args))
+	slog.Error(fmt.Sprintf(baseMsg, args...), baseMsg, args...)
 }
 */
--- utils/logger_test.go
+++ utils/logger_test.go
@@ -1,15 +1,48 @@
 package utils
 
 import (
-  "github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/assert"
 	"testing"
+  "encoding/json"
 )
 
-// TestLogInfo tests whether logs work with informational severity
-func TestLoggerScratchpad(t *testing.T) {
-  testString := "foobar"
-  logger := NewLogger(testString)
-  level := logger.GetLevel()
-  assert.EqualValues(t, level, testString)
+// testDirPath := "./testdata/tmp-logging-tests"
+
+func isJSON(str string) bool {
+    var js json.RawMessage
+    return json.Unmarshal([]byte(str), &js) == nil
 }
 
+// check that the context is being properly set & retrived
+func TestLoggerFormat(t *testing.T) {
+	log := NewLogger("JSON", 0)
+  log.Info("test log in json-mode", "foo", "bar")
+  // does it look good?
+	log = NewLogger("Text", 0)
+  log.Info("test log in text-mode", "foo", "bar")
+}
+
+/*
+// check that the context is being properly set & retrived
+func TestLoggerContext(t *testing.T) {
+	context := "foo"
+	logger := NewLogger(context, Info)
+	savedContext := logger.GetContext()
+	assert.EqualValues(t, savedContext, context)
+	context = "bar"
+  logger.SetContext(context)
+	savedContext = logger.GetContext()
+	assert.EqualValues(t, savedContext, context)
+}
+
+// TestLogInfo tests whether logs work with informational severity
+func TestLoggerLevel(t *testing.T) {
+	maxLevel := Warn 
+	logger := NewLogger("foobar", maxLevel)
+	gottenLevel := logger.GetMaxLevel()
+	assert.EqualValues(t, gottenLevel, maxLevel)
+  logger.Debug("debug log") // should not log
+  logger.Info("informational log") // should not log
+  logger.Warn("warning log") // should log
+}
+*/
