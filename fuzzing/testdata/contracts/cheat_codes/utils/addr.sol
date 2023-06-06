fca903b66c87f616816c4fafdfbfabbc612a2761
anishnaik <anish.naik@trailofbits.com>
2023-06-06 15:30:11 -0400

    Fix addr cheatcode (#155)
    - Fix `addr` and `sign` cheatcodes to correctly left-pad private keys that do not fill up 32-byte slices

--- fuzzing/testdata/contracts/cheat_codes/utils/addr.sol
+++ fuzzing/testdata/contracts/cheat_codes/utils/addr.sol
@@ -8,11 +8,23 @@ contract TestContract {
         // Obtain our cheat code contract reference.
         CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-        uint256 privateKey = 0x6df21769a2082e03f7e21f6395561279e9a7feb846b2bf740798c794ad196e00;
-        address expectedAddress = 0xdf8Ef652AdE0FA4790843a726164df8cf8649339;
+        // Test with random private key
+        uint256 pkOne = 0x6df21769a2082e03f7e21f6395561279e9a7feb846b2bf740798c794ad196e00;
+        address addrOne = 0xdf8Ef652AdE0FA4790843a726164df8cf8649339;
+        address result = cheats.addr(pkOne);
+        assert(result == addrOne);
 
-        // Call cheats.addr
-        address result = cheats.addr(privateKey);
-        assert(result == expectedAddress);
+        // Test with private key that requires padding
+        uint256 pkTwo = 1;
+        address addrTwo = 0x7E5F4552091A69125d5DfCb7b8C2659029395Bdf;
+        result = cheats.addr(pkTwo);
+        assert(result == addrTwo);
+
+        // Test with zero
+        uint256 pkThree = 0;
+        cheats.addr(pkThree);
+        // A private key of zero is not allowed so if we hit this assertion, then cheats.addr() did not revert which
+        // is incorrect
+        assert(false);
     }
 }
