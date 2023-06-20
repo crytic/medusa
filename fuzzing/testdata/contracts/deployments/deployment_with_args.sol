bfa37afccf3b582d553dbaa335ee97b4e153da09
anishnaik <anish.r.naik@gmail.com>
2023-06-20 18:25:07 -0400

    update property tests to be view functions

--- fuzzing/testdata/contracts/deployments/deployment_with_args.sol
+++ fuzzing/testdata/contracts/deployments/deployment_with_args.sol
@@ -15,15 +15,15 @@ contract DeploymentWithArgs {
         z = _z;
     }
 
-    function fuzz_checkX() public returns (bool) {
+    function fuzz_checkX() public view returns (bool) {
         return x != 123456789;
     }
 
-    function fuzz_checkY() public returns (bool) {
+    function fuzz_checkY() public view returns (bool) {
         return y != 0x5465;
     }
 
-    function fuzz_checkZ() public returns (bool) {
+    function fuzz_checkZ() public view returns (bool) {
         return z.a != 0x4d2;
     }
 
@@ -40,7 +40,7 @@ contract Dependent {
         deployed = _deployed;
     }
 
-    function fuzz_checkDeployed() public returns (bool) {
+    function fuzz_checkDeployed() public view returns (bool) {
         return deployed == 0x0000000000000000000000000000000000000000;
     }
 
