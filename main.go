f0e1cba6097a75f75c5954162f2e85843349c20d
anishnaik <anish.r.naik@gmail.com>
2023-07-06 11:39:14 -0400

    fix main

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/crytic/medusa/cmd"
 	"os"
 )
@@ -10,10 +9,7 @@ func main() {
 	// Run our root CLI command, which contains all underlying command logic and will handle parsing/invocation.
 	err := cmd.Execute()
 
-	// Print any error we encountered
 	if err != nil {
-		// TODO: Replace this when we have an appropriate logger in place.
-		fmt.Printf("ERROR:\n%s", err.Error())
 		os.Exit(1)
 	}
 }
