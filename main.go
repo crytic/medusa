e4c01ad0756dde5d1c77a031a9d0d0e157fc76bf
Anish Naik <anish.naik@trailofbits.com>
2023-10-12 15:28:36 -0400

    add pprof listener

--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/crytic/medusa/cmd"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 )
 
 func main() {
+	go func() {
+		http.ListenAndServe("localhost:8080", nil)
+	}()
+
 	// Run our root CLI command, which contains all underlying command logic and will handle parsing/invocation.
 	err := cmd.Execute()
 
