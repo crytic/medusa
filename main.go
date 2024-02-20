dcb23df9302c4d84355275a100e9737c0fd74e72
David Pokora <dpokora@gmail.com>
2024-02-20 14:37:39 -0500

    Added a unique exit code for failed tests, added generic way to bubble up errors with exit codes from cmd, organized exit code definitions into one package

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/crytic/medusa/cmd"
+	"github.com/crytic/medusa/cmd/exitcodes"
 	"os"
 )
 
@@ -9,7 +10,6 @@ func main() {
 	// Run our root CLI command, which contains all underlying command logic and will handle parsing/invocation.
 	err := cmd.Execute()
 
-	if err != nil {
-		os.Exit(1)
-	}
+	// Determine the exit code from any potential error and exit out.
+	os.Exit(exitcodes.GetErrorExitCode(err))
 }
