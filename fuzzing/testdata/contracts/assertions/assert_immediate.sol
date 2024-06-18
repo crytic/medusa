39f7d8e49b2bd1d9d153122992070cbb53840ce7
Sanan Hasanov <sanan.hasanov@Sanans-MacBook-Pro.local>
2024-06-18 10:04:04 -0400

    Modify existing contract to fit our test case

--- fuzzing/testdata/contracts/assertions/assert_immediate.sol
+++ fuzzing/testdata/contracts/assertions/assert_immediate.sol
@@ -1,6 +1,33 @@
+pragma solidity ^0.8.0;
+
+// This contract includes a function that we will call from TestContract.
+contract AnotherContract {
+    // This function doesn't need to do anything specific for this example.
+    function externalFunction() public pure returns (string memory) {
+        return "External function called";
+    }
+}
+
 // This contract ensures the fuzzer can encounter an immediate assertion failure.
 contract TestContract {
+    AnotherContract public anotherContract;
+
+    event ValueReceived(uint indexed value, uint second_val);
+    event ValueNonIndexedReceived(uint firstval, uint secondval);
+
+    // Deploy AnotherContract within the TestContract
+    constructor() {
+        anotherContract = new AnotherContract();
+    }
+
     function callingMeFails(uint value) public {
+        // Call the external function in AnotherContract.
+        anotherContract.externalFunction();
+        uint second_val = 5;
+
+        emit ValueReceived(value, second_val);
+        emit ValueNonIndexedReceived(1337, 5555);
+
         // ASSERTION: We always fail when you call this function.
         assert(false);
     }
