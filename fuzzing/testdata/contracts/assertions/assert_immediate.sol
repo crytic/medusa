be7c5b10a4b0f342a50b4c973fb17cc78e3c7da5
s4nsec <s4nsec@gmail.com>
2024-06-27 12:55:37 -0400

    Emit and return values of various types
    Random value generator functions are used to generate values during
    runtime as constants in the contract are already extracted and added to
    base value set via AST parsing.

--- fuzzing/testdata/contracts/assertions/assert_immediate.sol
+++ fuzzing/testdata/contracts/assertions/assert_immediate.sol
@@ -12,12 +12,45 @@ contract AnotherContract {
 contract TestContract {
     AnotherContract public anotherContract;
 
-    event ValueReceived(uint indexed value, uint second_val);
-    event ValueNonIndexedReceived(uint firstval, uint secondval);
+    bytes private constant _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    function internalFunction() public returns (string memory) {
-        anotherContract.externalFunction();
-        return "Internal function called";
+    function random(uint seed) internal view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp, block.difficulty, seed)));
+    }
+
+    function generateRandomString(uint seed) public view returns (string memory) {
+        bytes memory result = new bytes(6);
+        uint rand = random(seed);
+
+        for (uint i = 0; i < 6; i++) {
+            result[i] = _chars[rand % _chars.length];
+            rand = rand / _chars.length;
+        }
+
+        return string(result);
+    }
+
+    function generateRandomByteArray(uint seed, uint length) public view returns (bytes memory) {
+        bytes memory result = new bytes(length);
+        uint rand = random(seed);
+
+        for (uint i = 0; i < length; i++) {
+            result[i] = _chars[rand % _chars.length];
+            rand = rand / _chars.length;
+        }
+
+        return result;
+    }
+
+    event ValueReceived(uint indexed value, uint second_val, string test, bytes byteArray);
+    event ValueNonIndexedReceived(uint firstval, uint secondval, bytes1 myByte);
+
+    function internalFunction() public returns (string memory, string memory, uint, bytes1, bytes memory) {
+        string memory internalString = generateRandomString(444);
+        uint randInt = 4+14;
+        bytes1 randByte = 0x44;
+        bytes memory randBytes = generateRandomByteArray(555, 11);
+        return ("Internal function called", internalString, randInt, randByte, randBytes);
     }
 
     // Deploy AnotherContract within the TestContract
@@ -33,10 +66,15 @@ contract TestContract {
         // Call the external function in AnotherContract.
         anotherContract.externalFunction();
 
+        string memory randString = generateRandomString(123);
+        bytes memory byteArray = generateRandomByteArray(456, 10);
+
         uint second_val = 2+12;
 
-        emit ValueReceived(value, second_val);
-        emit ValueNonIndexedReceived(111+111, 444+444);
+        bytes1 myByte = 0x51;
+
+        emit ValueReceived(value, second_val, randString, byteArray);
+        emit ValueNonIndexedReceived(111+111, 444+444, myByte);
 
         // ASSERTION: We always fail when you call this function.
         assert(false);
