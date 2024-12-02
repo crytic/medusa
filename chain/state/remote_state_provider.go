8b6e8f7b3005c1cfa1c96134934bc857dffae75d
Benjamin Samuels <1222451+bsamuels453@users.noreply.github.com>
2024-12-02 14:27:34 -0800

    deployed contract optimization

--- chain/state/remote_state_provider.go
+++ chain/state/remote_state_provider.go
@@ -17,15 +17,20 @@ type RemoteStateProvider struct {
 
 	stateObjsImported  map[common.Address]struct{}
 	stateSlotsImported map[common.Address]map[common.Hash]struct{}
+
+	contractsDeployed           map[common.Address]struct{}
+	contractsDeployedBySnapshot map[int][]common.Address
 }
 
 func newRemoteStateProvider(stateQuerier StateBackend) *RemoteStateProvider {
 	return &RemoteStateProvider{
-		stateQuerier:        stateQuerier,
-		stateObjBySnapshot:  make(map[int][]common.Address),
-		stateSlotBySnapshot: make(map[int]map[common.Address][]common.Hash),
-		stateObjsImported:   make(map[common.Address]struct{}),
-		stateSlotsImported:  make(map[common.Address]map[common.Hash]struct{}),
+		stateQuerier:                stateQuerier,
+		stateObjBySnapshot:          make(map[int][]common.Address),
+		stateSlotBySnapshot:         make(map[int]map[common.Address][]common.Hash),
+		stateObjsImported:           make(map[common.Address]struct{}),
+		stateSlotsImported:          make(map[common.Address]map[common.Hash]struct{}),
+		contractsDeployed:           make(map[common.Address]struct{}),
+		contractsDeployedBySnapshot: make(map[int][]common.Address),
 	}
 }
 
@@ -50,6 +55,14 @@ func (s *RemoteStateProvider) ImportStateObject(addr common.Address, snapId int)
 }
 
 func (s *RemoteStateProvider) ImportStorageAt(addr common.Address, slot common.Hash, snapId int) (common.Hash, *state.RemoteStorageError) {
+	// if the contract was deployed locally, the RPC will not have data for its slots
+	if _, exists := s.contractsDeployed[addr]; exists {
+		return common.Hash{}, &state.RemoteStorageError{
+			CannotQueryDirtySlot: true,
+			Error:                fmt.Errorf("state slot %s of address %s cannot be remote-queried because the contract was deployed locally", slot.Hex(), addr.Hex()),
+		}
+	}
+
 	imported := s.isStateSlotImported(addr, slot)
 	if imported {
 		return common.Hash{}, &state.RemoteStorageError{
@@ -73,9 +86,14 @@ func (s *RemoteStateProvider) MarkSlotWritten(addr common.Address, slot common.H
 	s.recordImportedStateSlot(addr, slot, snapId)
 }
 
+func (s *RemoteStateProvider) MarkContractDeployed(addr common.Address, snapId int) {
+	s.recordContractDeployed(addr, snapId)
+}
+
 func (s *RemoteStateProvider) NotifyRevertedToSnapshot(snapId int) {
 	// purge all records down to and not including the provided snapId
 
+	/* accounts */
 	accountsToClear := make([]common.Address, 0)
 	for sId, accounts := range s.stateObjBySnapshot {
 		if sId > snapId {
@@ -87,6 +105,7 @@ func (s *RemoteStateProvider) NotifyRevertedToSnapshot(snapId int) {
 		delete(s.stateObjsImported, addr)
 	}
 
+	/* state slots */
 	accountSlotsToClear := make(map[common.Address][]common.Hash)
 	for sId, accounts := range s.stateSlotBySnapshot {
 		if sId > snapId {
@@ -104,6 +123,18 @@ func (s *RemoteStateProvider) NotifyRevertedToSnapshot(snapId int) {
 		for _, slot := range slots {
 			delete(s.stateSlotsImported[addr], slot)
 		}
+	}
+
+	/* contract deploys */
+	contractsToClear := make([]common.Address, 0)
+	for sId, contracts := range s.contractsDeployedBySnapshot {
+		if sId > snapId {
+			contractsToClear = append(contractsToClear, contracts...)
+			delete(s.contractsDeployedBySnapshot, sId)
+		}
+	}
+	for _, contract := range contractsToClear {
+		delete(s.contractsDeployed, contract)
 	}
 }
 
@@ -139,4 +170,12 @@ func (s *RemoteStateProvider) recordImportedStateSlot(addr common.Address, slot 
 		s.stateSlotBySnapshot[snapId][addr] = make([]common.Hash, 0)
 	}
 	s.stateSlotBySnapshot[snapId][addr] = append(s.stateSlotBySnapshot[snapId][addr], slot)
+}
+
+func (s *RemoteStateProvider) recordContractDeployed(addr common.Address, snapId int) {
+	s.contractsDeployed[addr] = struct{}{}
+	if _, ok := s.contractsDeployedBySnapshot[snapId]; !ok {
+		s.contractsDeployedBySnapshot[snapId] = make([]common.Address, 0)
+	}
+	s.contractsDeployedBySnapshot[snapId] = append(s.contractsDeployedBySnapshot[snapId], addr)
 }
