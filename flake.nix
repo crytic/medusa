faf128b0662915b6ad111d0ad2c5a098029dbd45
Emilio López <emilio.lopez@trailofbits.com>
2025-01-02 13:58:06 +0100

    Update nix flake references

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Medusa smart-contract fuzzer";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,7 +12,7 @@
         pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
         pyCommon = {
           format = "pyproject";
-          nativeBuildInputs = with pkgs.python39Packages; [ pythonRelaxDepsHook ];
+          nativeBuildInputs = with pkgs.python3Packages; [ pythonRelaxDepsHook ];
           pythonRelaxDeps = true;
           doCheck = false;
         };
@@ -21,28 +21,28 @@
 
         packages = rec {
 
-          solc-select = pkgs.python39Packages.buildPythonPackage (pyCommon // {
+          solc-select = pkgs.python3Packages.buildPythonPackage (pyCommon // {
             pname = "solc-select";
-            version = "1.0.3";
+            version = "1.0.4";
             src = builtins.fetchGit {
-              url = "git+ssh://git@github.com/crytic/solc-select";
-              rev = "97f160611c39d46e27d6f44a5a61344e6218d584";
+              url = "https://github.com/crytic/solc-select.git";
+              rev = "8072a3394bdc960c0f652fb72e928a7eae3631da";
             };
-            propagatedBuildInputs = with pkgs.python39Packages; [
+            propagatedBuildInputs = with pkgs.python3Packages; [
               packaging
               setuptools
               pycryptodome
             ];
           });
 
-          crytic-compile = pkgs.python39Packages.buildPythonPackage (pyCommon // rec {
+          crytic-compile = pkgs.python3Packages.buildPythonPackage (pyCommon // rec {
             pname = "crytic-compile";
-            version = "0.3.1";
+            version = "0.3.7";
             src = builtins.fetchGit {
-              url = "git+ssh://git@github.com/crytic/crytic-compile";
-              rev = "10104f33f593ab82ba5780a5fe8dd26385acd1c1";
+              url = "https://github.com/crytic/crytic-compile.git";
+              rev = "20df04f37af723eaa7fa56dc2c80169776f3bc4d";
             };
-            propagatedBuildInputs = with pkgs.python39Packages; [
+            propagatedBuildInputs = with pkgs.python3Packages; [
               cbor2
               pycryptodome
               setuptools
@@ -50,34 +50,31 @@
             ];
           });
 
-          slither = pkgs.python39Packages.buildPythonPackage (pyCommon // rec {
+          slither = pkgs.python3Packages.buildPythonPackage (pyCommon // rec {
             pname = "slither";
-            version = "0.9.3";
+            version = "0.10.4";
             format = "pyproject";
             src = builtins.fetchGit {
-              url = "git+ssh://git@github.com/crytic/slither";
-              rev = "e6b8af882c6419a9119bec5f4cfff93985a92f4e";
+              url = "https://github.com/crytic/slither.git";
+              rev = "aeeb2d368802844733671e35200b30b5f5bdcf5c";
             };
-            nativeBuildInputs = with pkgs.python39Packages; [ pythonRelaxDepsHook ];
+            nativeBuildInputs = with pkgs.python3Packages; [ pythonRelaxDepsHook ];
             pythonRelaxDeps = true;
             doCheck = false;
-            propagatedBuildInputs = with pkgs.python39Packages; [
+            propagatedBuildInputs = with pkgs.python3Packages; [
               packaging
               prettytable
               pycryptodome
               packages.crytic-compile
+              web3
             ];
-            postPatch = ''
-              echo "web3 dependency depends on ipfs which is bugged, removing it from the listed deps"
-              sed -i 's/"web3>=6.0.0",//' setup.py
-            '';
           });
 
           medusa = pkgs.buildGoModule {
             pname = "medusa";
-            version = "0.1.0"; # from cmd/root.go
+            version = "0.1.8"; # from cmd/root.go
             src = ./.;
-            vendorSha256 = "sha256-odBzty8wgFfdSF18D15jWtUNeQPJ7bkt9k5dx+8EFb4=";
+            vendorHash = "sha256-12Xkg5dzA83HQ2gMngXoLgu1c9KGSL6ly5Qz/o8U++8=";
             nativeBuildInputs = [
               packages.crytic-compile
               pkgs.solc
@@ -114,7 +111,6 @@
               go-tools
               gopls
               go-outline
-              gocode
               gopkgs
               gocode-gomod
               godef
