8cfbcbb42c431cfed1a4d5c0212fb8a0cbee4dec
Anish Naik <anish.naik@trailofbits.com>
2025-01-30 15:06:00 -0500

    update test case

--- fuzzing/testdata/contracts/cheat_codes/vm/label.sol
+++ fuzzing/testdata/contracts/cheat_codes/vm/label.sol
@@ -5,21 +5,24 @@ interface CheatCodes {
 
 contract LabelContract {
     function testLabel() public {
+        // Throw an assertion failure so that we can capture the execution trace
         assert(false);
     }
 }
 
 contract TestContract {
+    // Obtain our cheat code contract reference.
+    CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    // Create a contract
+    LabelContract alice = new LabelContract();
+
+    constructor() {
+        // Set label for LabelContract to "Alice"
+        cheats.label(address(alice), "Alice");
+    }
 
     function test() public {
-        // Obtain our cheat code contract reference.
-        CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-
-        // Create a contract
-        LabelContract alice = new LabelContract();
-
-        // set label and verify.
-        cheats.label(address(alice), "Alice");
+        // Call the label contract
         alice.testLabel();
     }
 }
