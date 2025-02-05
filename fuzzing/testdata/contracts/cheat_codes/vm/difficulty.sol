6f5f0a95629661ee51e3b1cf96418463ebbbf56b
Anish Naik <anish.naik@trailofbits.com>
2025-02-05 14:14:50 -0500

    last update

--- fuzzing/testdata/contracts/cheat_codes/vm/difficulty.sol
+++ fuzzing/testdata/contracts/cheat_codes/vm/difficulty.sol
@@ -8,7 +8,6 @@ contract TestContract {
         // Obtain our cheat code contract reference.
         CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
-        // Use try catch
         uint256 originalDifficulty = block.difficulty;
         // Update the difficulty
         cheats.difficulty(x);
