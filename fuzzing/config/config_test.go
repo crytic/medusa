28a32e453b50c9e4c52b31bd8b18b7e8409b6d57
Anish Naik <anish.naik@trailofbits.com>
2025-03-12 14:10:16 -0400

    improve commenting.

--- fuzzing/config/config_test.go
+++ fuzzing/config/config_test.go
@@ -6,7 +6,9 @@ import (
 	"testing"
 )
 
-func TestDeserializeBalances(t *testing.T) {
+// TestUnmarshalBalances will test the unmarshalling of a ContractBalance from a string
+func TestUnmarshalBalances(t *testing.T) {
+	// Create the list of test cases
 	testCases := []struct {
 		input           string
 		expectedBalance ContractBalance
@@ -21,6 +23,7 @@ func TestDeserializeBalances(t *testing.T) {
 		{"\"0X10\"", ContractBalance{*big.NewInt(16)}},
 	}
 
+	// Iterate over the test cases and unmarshal the input string into a ContractBalance
 	for _, tc := range testCases {
 		var b ContractBalance
 		if err := json.Unmarshal([]byte(tc.input), &b); err != nil {
@@ -32,7 +35,9 @@ func TestDeserializeBalances(t *testing.T) {
 	}
 }
 
-func TestSerializeBalances(t *testing.T) {
+// TestMarshalBalances will test the marshalling of a ContractBalance to a string
+func TestMarshalBalances(t *testing.T) {
+	// Create the list of test cases
 	testCases := []struct {
 		input           ContractBalance
 		expectedBalance string
@@ -42,6 +47,7 @@ func TestSerializeBalances(t *testing.T) {
 		{ContractBalance{*big.NewInt(0).Mul(big.NewInt(1000000000000000000), big.NewInt(1000000000000000000))}, "\"1000000000000000000000000000000000000\""},
 	}
 
+	// Iterate over the test cases and marshal the ContractBalance to a string
 	for _, tc := range testCases {
 		out, err := json.Marshal(tc.input)
 		if err != nil {
